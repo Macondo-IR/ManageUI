b82ff555c8a4b1f54136b86f24a083f44ae7d9d9
hossein <hossein.a365@gmail.com>
2021-06-28 06:53:55 +0430

    add axios

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+   const [activities, setActivities] = useState([]);
+   useEffect(() => {
+     axios.get('http://localhost:5000/api/activities').then(response =>{
+       setActivities(response.data);
+     })
+   }, [])
+   
   return (
     <div className="App">
       <header className="App-header">
