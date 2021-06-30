e6b92a872daa82d222f8ded46a9927d8a2431d7d
hossein <hossein.a365@gmail.com>
2021-06-30 06:34:46 +0430

    final code of cumming

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Router } from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+import 'react-toastify/dist/ReactToastify.min.css';
+// import 'react-widgets/styles.css';
+import 'react-widgets/dist/css/react-widgets.css';
+
+import './app/layout/styles.css';
+import App from './app/layout/App';
+import ScrollToTop from './app/layout/ScrollToTop';
 import reportWebVitals from './reportWebVitals';
+import dateFnsLocalizer from 'react-widgets-date-fns';
+
+
+// ReactDOM.render(
+//     <App />
+//   ,
+//   document.getElementById('root')
+// );
+
+
+
+dateFnsLocalizer();
+
+export const history = createBrowserHistory();
 
 ReactDOM.render(
-    <App />
-  ,
+  <Router history={history}>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
+  </Router>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
 reportWebVitals();
