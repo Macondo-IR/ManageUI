2b8dc476afc5ef0a073bb18b2563b632c3f97d79
hossein <hossein.a365@gmail.com>
2021-07-02 13:20:11 +0430

    06

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,24 +3,17 @@ import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.min.css';
 import 'react-widgets/dist/css/react-widgets.css';
-import {createBrowserHistory} from 'history';
+import { BrowserRouter } from 'react-router-dom';
 
 import './app/layout/styles.css';
 import App from './app/layout/App';
-import reportWebVitals from './reportWebVitals';
 
+
+
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-
-// export const history = createBrowserHistory();
-
-// ReactDOM.render(
-//   <Router history={history}>
-//     <ScrollToTop>
-//       <App />
-//     </ScrollToTop>
-//   </Router>,
-//   document.getElementById('root')
-// );
+  
+ 
 reportWebVitals();
